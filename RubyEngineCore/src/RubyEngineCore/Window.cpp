315663d9d213e46bb908828361a0b69b9088b79d
Vitaliy20O6 <shalapinv6@gmail.com>
2024-03-22 14:05:30 +0700

    Vertex Array Object

--- RubyEngineCore/src/RubyEngineCore/Window.cpp
+++ RubyEngineCore/src/RubyEngineCore/Window.cpp
@@ -2,6 +2,7 @@
 #include "RubyEngineCore/Logs.hpp"
 #include "RubyEngineCore/Rendering/OpenGL/ShaderProgram.hpp"
 #include "RubyEngineCore/Rendering/OpenGL/VertexBuffer.hpp"
+#include "RubyEngineCore/Rendering/OpenGL/VertexArray.hpp"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -47,8 +48,7 @@ namespace RubyEngine
     std::unique_ptr<ShaderProgram> p_shader_program;
     std::unique_ptr<VertexBuffer> p_points_vbo;
     std::unique_ptr<VertexBuffer> p_colors_vbo;
-    GLuint vao;
-    
+    std::unique_ptr<VertexArray> p_vao;    
 
 	Window::Window(std::string title, const unsigned int width, const unsigned int height)
         :m_data({ std::move(title), width, height })
@@ -72,7 +72,7 @@ namespace RubyEngine
         glClear(GL_COLOR_BUFFER_BIT);
 
         p_shader_program->bind();
-        glBindVertexArray(vao);
+        p_vao->bind();
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
@@ -176,18 +176,10 @@ namespace RubyEngine
 
         p_points_vbo = std::make_unique<VertexBuffer>(points, sizeof(points));
         p_colors_vbo = std::make_unique<VertexBuffer>(colors, sizeof(colors));
-
-        glGenVertexArrays(1,&vao);
-        glBindVertexArray(vao);
-
-        glEnableVertexAttribArray(0);
-        p_points_vbo->bind();
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        glEnableVertexAttribArray(1);
-        p_colors_vbo->bind();
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
+        p_vao = std::make_unique<VertexArray>();
+        
+        p_vao->add_buffer(*p_points_vbo);
+        p_vao->add_buffer(*p_colors_vbo);
         return 0;
 	}
 	void Window::shutdown()
