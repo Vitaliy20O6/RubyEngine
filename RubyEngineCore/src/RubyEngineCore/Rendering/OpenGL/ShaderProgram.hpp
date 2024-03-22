0c5142ce390d35c18a8321b1c4c4b5967eec552d
Vitaliy20O6 <shalapinv6@gmail.com>
2024-03-22 13:15:21 +0700

    Shaders

---
+++ RubyEngineCore/src/RubyEngineCore/Rendering/OpenGL/ShaderProgram.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace RubyEngine {
+
+    class ShaderProgram
+    {
+    private:
+        bool m_isCompiled = false;
+        unsigned int m_id = 0;    
+
+    public:
+        ShaderProgram(const char* vertex_shader_src, const char* fragment_shader_src);
+        ShaderProgram(ShaderProgram&&);
+        ShaderProgram& operator=(ShaderProgram&&);
+        ~ShaderProgram();
+
+        ShaderProgram() = delete;
+        ShaderProgram(const ShaderProgram&) = delete;
+        ShaderProgram& operator=(const ShaderProgram&) = delete;
+
+        void bind() const;
+        static void unbind();
+        bool isCompiled() const { return m_isCompiled; }
+
+
+    };
+
+}
